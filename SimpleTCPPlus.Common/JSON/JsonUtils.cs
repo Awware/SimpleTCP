595abd2e1c8e7b7fcd18a659c07b8093fedb6392
Unknown <38399178+Awware@users.noreply.github.com>
2020-01-02 14:51:43 +0600

    Data string to byte[]

--- SimpleTCPPlus.Common/JSON/JsonUtils.cs
+++ SimpleTCPPlus.Common/JSON/JsonUtils.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json;
+using System.Text;
 
 namespace SimpleTCPPlus.Common.JSON
 {
     public static class JsonUtils
     {
+        public static T DeserializeIt<T>(byte[] data) => (T)JsonConvert.DeserializeObject<T>(Encoding.Default.GetString(data));
         public static T DeserializeIt<T>(string data) => (T)JsonConvert.DeserializeObject<T>(data);
         public static string SerializeIt(object obj) => JsonConvert.SerializeObject(obj);
     }
--- SimpleTCPPlus.Common/Packet.cs
+++ SimpleTCPPlus.Common/Packet.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Text;
 
 namespace SimpleTCPPlus.Common
 {
     [Serializable]
     public class Packet
     {
-        public Packet(string Data, string TYPE/*, string FINGER*/) { RawData = Data; PacketType = TYPE; /*Fingerprint = FINGER; */}
-        public string RawData { get; }
+        public Packet(string Data, string TYPE) { RawData = Encoding.Default.GetBytes(Data); PacketType = TYPE; }
+        public Packet(byte[] Data, string TYPE/*, string FINGER*/) { RawData = Data; PacketType = TYPE; /*Fingerprint = FINGER; */}
+        public byte[] RawData { get; }
         public string PacketType { get; }
         //public string Fingerprint { get; }
     }
